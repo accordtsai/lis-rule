1714ca811325e8ac6fc26e93ec9e8575038f9e18
changyiyen <changyiyen@gmail.com>
2015-08-08 21:08:44 +0800

    Update to AST module
    Various bugfixes

--- modules/lisanalyze_ast.py
+++ modules/lisanalyze_ast.py
@@ -38,55 +38,56 @@ def analyze(file_name, lis_struct, time, args):
     else:
         return None
 
-    # Unit conversion; disabled due to the IU not being defined in Pint
-    #if args.convert:
-    #    import pint
-    #    ureg = pint.UnitRegistry()
-    #    if "AST" in lis_struct[time].keys():
-    #        factor = (ureg.parse_expression(lis_struct[time]["AST"]["unit"])).to(__unit).magnitude
-    #    ast_val *= factor
+    # Unit conversion
+    if args.convert:
+        import pint
+        ureg = pint.UnitRegistry()
+        ureg.define('kat = 1 mol / s')
+        ureg.define('U = 1.657e-8 kat')
+        factor = (ureg.parse_expression(lis_struct[time]["AST"]["unit"])).to(__unit).magnitude
+        ast_val *= factor
 
-    # Out-of-normal-range warning (currently disabled); provided values take precedence
-    #if "ref_high" in lis_struct[time]["AST"].keys():
-    #    if ast_val > lis_struct[time]["AST"]["ref_high"]:
-    #        event_time = time
-    #        if file_name not in __event_dict.keys():
-    #            __event_dict[file_name] = {}
-    #        if event_time not in __event_dict[file_name].keys():
-    #            __event_dict[file_name][event_time] = []
-    #        event_str = "AST too high (current value {}; reference value {})".format(ast_val, lis_struct[time]["AST"]["ref_high"])
-    #        __event_dict[file_name][event_time].append(event_str)
-    #else:
-    #    if args.warn:
-    #        print("WARNING: higher reference value not provided; falling back to built-in value", file=sys.stderr)
-    #    if ast_val > __ast_ul:
-    #        event_time = time
-    #        if file_name not in __event_dict.keys():
-    #            __event_dict[file_name] = {}
-    #        if event_time not in __event_dict[file_name].keys():
-    #            __event_dict[file_name][event_time] = []
-    #        event_str = "AST too high (current value {}; reference value {})".format(ast_val, __ast_ul)
-    #        __event_dict[file_name][event_time].append(event_str)
-    #if "ref_low" in lis_struct[time]["AST"].keys():
-    #    if ast_val < lis_struct[time]["AST"]["ref_low"]:
-    #        event_time = time
-    #        if file_name not in __event_dict.keys():
-    #            __event_dict[file_name] = {}
-    #        if event_time not in __event_dict[file_name].keys():
-    #            __event_dict[file_name][event_time] = []
-    #        event_str = "AST too low (current value {}; reference value {})".format(ast_val, lis_struct[time]["AST"]["ref_low"])
-    #        __event_dict[file_name][event_time].append(event_str)
-    #else:
-    #    if args.warn:
-    #        print("WARNING: lower reference value not provided; falling back to built-in value", file=sys.stderr)
-    #    if ast_val < __ast_ul:
-    #        event_time = time
-    #        if file_name not in __event_dict.keys():
-    #            __event_dict[file_name] = {}
-    #        if event_time not in __event_dict[file_name].keys():
-    #            __event_dict[file_name][event_time] = []
-    #        event_str = "AST too low (current value {}; reference value {})".format(ast_val, __ast_ll)
-    #        __event_dict[file_name][event_time].append(event_str)
+    # Out-of-normal-range warning; provided values take precedence
+    if "ref_high" in lis_struct[time]["AST"].keys():
+        if lis_struct[time]["AST"]["lab_value"] > lis_struct[time]["AST"]["ref_high"]:
+            event_time = time
+            if file_name not in __event_dict.keys():
+                __event_dict[file_name] = {}
+            if event_time not in __event_dict[file_name].keys():
+                __event_dict[file_name][event_time] = []
+            event_str = "AST too high (current value {}; reference value {} ({}))".format(lis_struct[time]["AST"]["lab_value"], lis_struct[time]["AST"]["ref_high"], lis_struct[time]["AST"]["unit"])
+            __event_dict[file_name][event_time].append(event_str)
+    else:
+        if args.warn:
+            print("WARNING: higher reference value not provided; falling back to built-in value", file=sys.stderr)
+        if ast_val > __ast_ul:
+            event_time = time
+            if file_name not in __event_dict.keys():
+                __event_dict[file_name] = {}
+            if event_time not in __event_dict[file_name].keys():
+                __event_dict[file_name][event_time] = []
+            event_str = "AST too high (current value {}; reference value {} ({}))".format(ast_val, __ast_ul, __unit)
+            __event_dict[file_name][event_time].append(event_str)
+    if "ref_low" in lis_struct[time]["AST"].keys():
+        if lis_struct[time]["AST"]["lab_value"] < lis_struct[time]["AST"]["ref_low"]:
+            event_time = time
+            if file_name not in __event_dict.keys():
+                __event_dict[file_name] = {}
+            if event_time not in __event_dict[file_name].keys():
+                __event_dict[file_name][event_time] = []
+            event_str = "AST too low (current value {}; reference value {} ({}))".format(lis_struct[time]["AST"]["lab_value"], lis_struct[time]["AST"]["ref_low"], lis_struct[time]["AST"]["unit"])
+            __event_dict[file_name][event_time].append(event_str)
+    else:
+        if args.warn:
+            print("WARNING: lower reference value not provided; falling back to built-in value", file=sys.stderr)
+        if ast_val < __ast_ll:
+            event_time = time
+            if file_name not in __event_dict.keys():
+                __event_dict[file_name] = {}
+            if event_time not in __event_dict[file_name].keys():
+                __event_dict[file_name][event_time] = []
+            event_str = "AST too low (current value {}; reference value {} ({}))".format(ast_val, __ast_ll, __unit)
+            __event_dict[file_name][event_time].append(event_str)
 
     # AST > 1000 suggests ischemia, viral infection, toxicity (Lange Pocket Guide to Diagnostic Tests, 6e, p.531)
     if ast_val > 1000:
@@ -95,22 +96,36 @@ def analyze(file_name, lis_struct, time, args):
             __event_dict[file_name] = {}
         if event_time not in __event_dict[file_name].keys():
             __event_dict[file_name][event_time] = []
-        event_str = "AST markedly elevated ({} ({})); consider ischemia, infection, toxicity".format(ast_val, lis_struct[time]["AST"]["unit"])
+        event_str = "AST markedly elevated ({} ({})); consider ischemia, infection, toxicity".format(ast_val, __unit)
         __event_dict[file_name][event_time].append(event_str)
     
     # AST / ALT > 2 suggests alcoholism (Lange Pocket Guide to Diagnostic Tests, 6e, p.531)
-    #import lisanalyze_alt
-    #alt_passthrough = lisanalyze_alt.passthrough(file_name, lis_struct, time, args)
-    #if alt_passthrough:
-    #    alt, unit = alt_passthrough
-    #    if (ast_val / alt) > 2:
-    #        event_time = time
-    #        if file_name not in __event_dict.keys():
-    #            __event_dict[file_name] = {}
-    #        if event_time not in __event_dict[file_name].keys():
-    #            __event_dict[file_name][event_time] = []
-    #        event_str = "AST/ALT > 2 (AST: {}, ALT: {}, AST/ALT: {}); consider alcoholic cirrhosis".format(ast_val, alt, ast_val/alt)
-    #        __event_dict[file_name][event_time].append(event_str)
+    import lisanalyze_alt
+    alt_passthrough = lisanalyze_alt.passthrough(file_name, lis_struct, time, args)
+    if alt_passthrough:
+        alt, unit = alt_passthrough
+        if (ast_val / alt) > 2:
+            event_time = time
+            if file_name not in __event_dict.keys():
+                __event_dict[file_name] = {}
+            if event_time not in __event_dict[file_name].keys():
+                __event_dict[file_name][event_time] = []
+            event_str = "AST/ALT > 2 (AST: {}, ALT: {}, AST/ALT: {}); consider alcoholic hepatitis".format(ast_val, alt, ast_val/alt)
+            __event_dict[file_name][event_time].append(event_str)
+
+    # AST / ALT > 1 suggests cirrhosis in patients with hepatitis C (Lange Pocket Guide to Diagnostic Tests, 6e, p.73)
+    import lisanalyze_alt
+    alt_passthrough = lisanalyze_alt.passthrough(file_name, lis_struct, time, args)
+    if alt_passthrough:
+        alt, unit = alt_passthrough
+        if ast_val > alt:
+            event_time = time
+            if file_name not in __event_dict.keys():
+                __event_dict[file_name] = {}
+            if event_time not in __event_dict[file_name].keys():
+                __event_dict[file_name][event_time] = []
+            event_str = "AST/ALT > 1 (AST: {}, ALT: {}, AST/ALT: {}); possible cirrhosis if patient has hepatitis C".format(ast_val, alt, ast_val/alt)
+            __event_dict[file_name][event_time].append(event_str)
 
     return False
 
@@ -149,7 +164,8 @@ def passthrough(file_name, lis_struct, time, args):
     if args.convert:
         import pint
         ureg = pint.UnitRegistry()
-        if "AST" in lis_struct[time].keys():
-            factor = (ureg.parse_expression(lis_struct[time]["AST"]["unit"])).to(__unit).magnitude
+        ureg.define('kat = 1 mol / s')
+        ureg.define('U = 1.657e-8 kat')
+        factor = (ureg.parse_expression(lis_struct[time]["AST"]["unit"])).to(__unit).magnitude
         ast_val *= factor
     return ast_val, __unit
